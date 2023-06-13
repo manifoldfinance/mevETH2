62c04a7a5eee6f80d1733854337332fc009ba650
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-06-12 20:32:19 -0600

    fixed tests

---
+++ src/interfaces/Errors.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.20;
+
+interface MevEthErrors {
+    /// Errors
+    error DepositFailed();
+    error InsufficientBufferedEth();
+    error TooManyValidatorRegistrations();
+    error ExceedsStakingAllowance();
+    error StakingPaused();
+    error NotEnoughEth();
+    error DepositTooLow();
+    error ZeroShares();
+    error ReportedBeaconValidatorsGreaterThanTotalValidators();
+    error ReportedBeaconValidatorsDecreased();
+    error BeaconDepositFailed();
+    error InvalidWithdrawalCredentials();
+    error OperatorsNotCommitted();
+    error OperatorMaxValidatorsReached();
+    error OperatorNotCommitted();
+    error MaxValidatorError();
+    error InvalidOperator();
+    error ValidatorPreviouslyRegistered();
+    error NotAuthorized();
+    error DepositTooSmall();
+    error InvalidSender();
+
+    error TransferExceedsAllowance();
+}
