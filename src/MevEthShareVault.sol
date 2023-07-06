76694b54ff58cba3da17ba9a9565549fb82948d9
0xKitsune <0xKitsune@protonmail.com>
2023-07-06 00:30:33 -0400

    updated MevEthShareVault

--- src/MevEthShareVault.sol
+++ src/MevEthShareVault.sol
@@ -6,38 +6,82 @@ import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { ITinyMevEth } from "./interfaces/ITinyMevEth.sol";
 import { IMevEthShareVault } from "./interfaces/IMevEthShareVault.sol";
+import { MevEthErrors } from "./interfaces/Errors.sol";
 
 /// @title MevEthShareVault
 /// @notice This contract controls the ETH Rewards earned by mevEth
 contract MevEthShareVault is Auth, IMevEthShareVault {
     using SafeTransferLib for ERC20;
 
-    receive() external payable { }
+    struct ProtocolBalance {
+        // Accrued fees above the median mev payment
+        uint128 fees;
+        // Accrued mev payments at or below the median mev payment
+        uint128 rewards;
+    }
 
-    fallback() external payable { }
-
-    ITinyMevEth public immutable MEV_ETH;
-
-    uint256 avgFeeRewardsPerBlock;
-
+    event ValidatorPayment(uint256 indexed blockNumber, address indexed coinbase, uint256 indexed amount);
+    event MevPayment(uint256 indexed blockNumber, address indexed coinbase, uint256 indexed amount);
     event TokenRecovered(address indexed recipient, address indexed token, uint256 indexed amount);
+
+    uint128 medianMevPayment;
+    uint128 medianValidatorPayment;
+    ProtocolBalance protocolBalance;
+    address immutable mevEth;
+    address feeTo;
 
     /// @notice Construction sets authority, MevEth, and averageFeeRewardsPerBlock
     /// @param authority The address of the controlling admin authority
-    /// @param mevEth The address of the WETH contract to use for deposits
-    /// @param initialFeeRewardsPerBlock TODO: add description for how this is used
+    /// @param _mevEth The address of the WETH contract to use for deposits
+    /// @param _feeTo TODO:
+    /// @param _medianMevPayment TODO:
+    /// @param _medianValidatorPayment TODO:
 
-    constructor(address authority, address mevEth, uint256 initialFeeRewardsPerBlock) Auth(authority) {
-        MEV_ETH = ITinyMevEth(mevEth);
-        avgFeeRewardsPerBlock = initialFeeRewardsPerBlock;
+    constructor(address authority, address _mevEth, address _feeTo, uint256 _medianMevPayment, uint128 _medianValidatorPayment) Auth(authority) {
+        mevEth = _mevEth;
+        medianMevPayment = _medianMevPayment;
+        medianValidatorPayment = _medianValidatorPayment;
     }
 
-    function payRewards(uint256 amount) external {
-        MEV_ETH.grantRewards{ value: amount }();
+    function payRewards() external {
+        //TODO: handle failure case and send to admin
+        ITinyMevEth(mevEth).grantRewards{ value: protocolBalance.rewards }();
+        protocolBalance.rewards = 0;
+    }
+
+    function setMedianValidatorPayment(uint256 newMedian) external onlyOperator {
+        //TODO: checks prior to updating
+        medianValidatorPayment = newMedian;
+    }
+
+    function setMedianMevPayment(uint256 newMedian) external onlyOperator {
+        //TODO: checks prior to updating
+        medianMevPayment = newMedian;
+    }
+
+    function setFeeTo(address newFeeTo) external onlyAdmin {
+        if (newFeeTo == address(0)) {
+            revert MevEthErrors.ZeroAddress();
+        }
+
+        feeTo = newFeeTo;
     }
 
     function recoverToken(address token, address recipient, uint256 amount) external onlyAdmin {
         ERC20(token).safeTransfer(recipient, amount);
         emit TokenRecovered(recipient, token, amount);
     }
+
+    receive() external payable {
+        if (msg.sender == block.coinbase) {
+            //TODO: handle as validator payment
+
+            //TODO: emit ValidatorPayment
+        } else {
+            //TODO: handle as mev payment
+            //TODO: emit MevPayment
+        }
+    }
+
+    fallback() external payable { }
 }
--- src/interfaces/IMevEthShareVault.sol
+++ src/interfaces/IMevEthShareVault.sol
@@ -3,8 +3,50 @@
 pragma solidity ^0.8.19;
 
 interface IMevEthShareVault {
-    function payRewards(uint256 amount) external;
 
-    // onlyAdmin Functions
+    struct ProtocolBalance{
+        // Accrued fees above the median mev payment
+        uint128 fees;
+        // Accrued mev payments at or below the median mev payment
+        uint128 rewards;
+    }
+
+    /* Function to handle mev payments, if the msg.sender is the validator, 
+    a ValditorPayment should be emitted and the fees/profits should be updated based on the median validator payment. 
+    Otherwise, a MevPayment should be emitted and the fees/profits should be updated based on the medianMevPayment.
+    */
+    function receive() external payable; 
+    
+    // Function to send rewards to MevEth Contract. In the case of failure, this function sends the funds to the Admin as a fallback.
+    function payRewards() external;
+    
+    // Two-Step Ownable, have the same commit scheme as mevEth
+    // TODO: @controlcpluscontrolv do we need these if we have admin updgrade functions?
+    function commitNewOwner(address newOwner) external;
+    function setNewOwner() external;
+    
+    // Getter functions for public variables
+    function feeTo() external view returns (address feeTo);
+    function mevEth() external view returns (address mevEth);
+    function admins(address admin) external view returns (bool);
+    function operators(address operator) external view returns (bool);
+    function protocolBalance() external view returns (ProtocolBalance memory protocolBalance);
+    function medianValidatorPayment() external view returns (uint256 median);
+    function medianMevPayment() external view returns (uint256 median);
+
+    // Admin controls
     function recoverToken(address token, address recipient, uint256 amount) external;
+    // Function to set the `feeTo` address
+    function setFeeTo(address newFeeTo) external;
+    // Send the protocol fees to the `feeTo` address
+    function sendFees() external;
+    function addAdmin(address newAdmin) external;
+    function deleteAdmin(address oldAdmin) external;
+    function addOperator(address newOperator) external;
+    function deleteOperator(address oldOperator) external;
+    
+    // Operator controls
+    function setMedianValidatorPayment(uint256 newMedian) external;
+    function setMedianMevPayment(uint256 newMedian) external;
+
 }
