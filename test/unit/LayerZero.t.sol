833b3c7fd5cda50dcdf228fb03d4731a98321312
sandybradley <sandy.bradley@gmail.com>
2023-07-14 15:21:56 +0200

    feat: switch OFTV2 for OFTV2WithFee

--- test/unit/LayerZero.t.sol
+++ test/unit/LayerZero.t.sol
@@ -10,8 +10,8 @@ contract LayerZeroTest is MevEthTest {
     string constant name = "Mev Liquid Staked Ether";
     string constant symbol = "mevETH";
 
-    OFTV2 public polyMevEth;
-    OFTV2 public arbMevEth;
+    OFTWithFee public polyMevEth;
+    OFTWithFee public arbMevEth;
     LZEndpointMock polygonEndpoint;
     LZEndpointMock arbitrumEndpoint;
 
@@ -25,7 +25,7 @@ contract LayerZeroTest is MevEthTest {
 
         // simulate deploy OFT on arbitrum
         arbitrumEndpoint = new LZEndpointMock(ARBITRUM_ID);
-        arbMevEth = new OFTV2(name, symbol, 18, 8, SamBacha, address(arbitrumEndpoint));
+        arbMevEth = new OFTWithFee(name, symbol, 18, 8, SamBacha, address(arbitrumEndpoint));
 
         // set trusted remotes
         vm.prank(SamBacha);
@@ -63,7 +63,7 @@ contract LayerZeroTest is MevEthTest {
         // call estimate send fee
         (uint256 nativeFee,) = mevEth.estimateSendFee(ARBITRUM_ID, _addressToBytes32(User02), amount, false, "");
         // send tokens from mainnet to simulated arbitrum
-        mevEth.sendFrom{ value: nativeFee }(User01, ARBITRUM_ID, _addressToBytes32(User02), amount, callParams);
+        mevEth.sendFrom{ value: nativeFee }(User01, ARBITRUM_ID, _addressToBytes32(User02), amount, amount, callParams);
 
         assertEq(arbMevEth.totalSupply(), amount);
         assertEq(arbMevEth.balanceOf(User02), amount);
@@ -78,10 +78,64 @@ contract LayerZeroTest is MevEthTest {
         // call estimate send fee
         (nativeFee,) = arbMevEth.estimateSendFee(ETH_ID, _addressToBytes32(User01), amount, false, "");
         // send tokens back to mainnet from simulated arbitrum
-        arbMevEth.sendFrom{ value: nativeFee }(User02, ETH_ID, _addressToBytes32(User01), amount, callParams);
+        arbMevEth.sendFrom{ value: nativeFee }(User02, ETH_ID, _addressToBytes32(User01), amount, amount, callParams);
 
         assertEq(mevEth.totalSupply(), amount);
         assertEq(mevEth.balanceOf(User01), amount);
+    }
+
+    function testQuoteFee() public {
+        // default fee 0%
+        assertEq(mevEth.quoteOFTFee(ETH_ID, 10_000), 0);
+
+        vm.prank(SamBacha);
+        // change default fee to 10%
+        mevEth.setDefaultFeeBp(1000);
+        assertEq(mevEth.quoteOFTFee(ETH_ID, 10_000), 1000);
+
+        vm.prank(SamBacha);
+        // change fee to 20% for chain 2
+        mevEth.setFeeBp(ARBITRUM_ID, true, 2000);
+        assertEq(mevEth.quoteOFTFee(ETH_ID, 10_000), 1000);
+        assertEq(mevEth.quoteOFTFee(ARBITRUM_ID, 10_000), 2000);
+
+        // change fee to 0% for chain 2
+        vm.prank(SamBacha);
+        mevEth.setFeeBp(ARBITRUM_ID, true, 0);
+        assertEq(mevEth.quoteOFTFee(ETH_ID, 10_000), 1000);
+        assertEq(mevEth.quoteOFTFee(ARBITRUM_ID, 10_000), 0);
+
+        vm.prank(SamBacha);
+        // disable fee for chain 2
+        mevEth.setFeeBp(ARBITRUM_ID, false, 0);
+        assertEq(mevEth.quoteOFTFee(ETH_ID, 10_000), 1000);
+        assertEq(mevEth.quoteOFTFee(ARBITRUM_ID, 10_000), 1000);
+    }
+
+    function testChargeSend() public {
+        uint256 amount = 1 ether;
+        vm.deal(User01, amount * 2);
+        vm.deal(User02, amount * 2);
+
+        vm.prank(SamBacha);
+        mevEth.setDefaultFeeBp(5000);
+
+        vm.startPrank(User01);
+        // Deposit 1 ETH into the mevETH contract
+        mevEth.deposit{ value: amount }(amount, User01);
+
+        // setup call params
+        ICommonOFT.LzCallParams memory callParams;
+        callParams.refundAddress = payable(User01);
+        callParams.zroPaymentAddress = address(0);
+        callParams.adapterParams = "";
+        // call estimate send fee
+        (uint256 nativeFee,) = mevEth.estimateSendFee(ARBITRUM_ID, _addressToBytes32(User02), amount, false, "");
+        // send tokens from mainnet to simulated arbitrum
+        mevEth.sendFrom{ value: nativeFee }(User01, ARBITRUM_ID, _addressToBytes32(User02), amount, amount / 2, callParams);
+
+        assertEq(arbMevEth.totalSupply(), amount / 2);
+        assertEq(arbMevEth.balanceOf(User02), amount / 2);
     }
 
     function _addressToBytes32(address _address) internal pure virtual returns (bytes32) {
