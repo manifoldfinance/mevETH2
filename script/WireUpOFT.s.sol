e2d3576004eefd074a3ec242c885137f90484200
sandybradley <sandy.bradley@gmail.com>
2023-07-15 10:13:32 +0200

    feat: wire-up trusted remotes and default fee

---
+++ script/WireUpOFT.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import { OFTWithFee } from "src/layerZero/oft/OFTWithFee.sol";
+
+contract WireUpOFTScript is Script {
+    error ListMismatch();
+
+    /// @notice Set trusted remotes for localMevEth
+    /// @dev only Admin
+    /// @param defaultFeeBp default fee for bridging out of 10,000 i.e. 2 = 0.02%
+    /// @param localMevEth local mevETH contract
+    /// @param lzChainIds list of remote lz chain ids to set trusted remotes for
+    /// @param remoteMevEths list of remote mevETH contracts to set trusted remotes for
+    function run(uint16 defaultFeeBp, address localMevETH, uint16[] calldata lzChainIds, address[] calldata remoteMevEths) public {
+        uint256 length = lzChainIds.length;
+        if (length != remoteMevEths.length) revert ListMismatch();
+        vm.startBroadcast();
+        for (uint256 i; i < length; i++) {
+            OFTWithFee(localMevETH).setTrustedRemote(lzChainIds[i], abi.encodePacked(remoteMevEths[i], localMevETH));
+        }
+        OFTWithFee(localMevETH).setDefaultFeeBp(defaultFeeBp);
+        vm.stopBroadcast();
+    }
+}
