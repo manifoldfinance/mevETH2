7592eeab79415a96616571e233c34a1e64474e02
sandybradley <sandy.bradley@gmail.com>
2023-07-28 11:02:31 +0200

    chore: move a16z tests into local repo for mods

--- test/unit/ERC4626-a16z.t.sol
+++ test/unit/ERC4626-a16z.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 import "forge-std/console.sol";
 import "../MevEthTest.sol";
 import "src/interfaces/Errors.sol";
-import { ERC4626Test as a16z } from "erc4626-tests/ERC4626.test.sol";
+import { ERC4626Test as a16z } from "../a16z/erc4626-tests/ERC4626.test.sol";
 
 contract ERC4626StdTest is a16z, MevEthTest {
     function setUp() public override(a16z, MevEthTest) {
