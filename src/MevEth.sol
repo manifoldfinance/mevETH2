6132f2e5c4ba5584c93ca04105431847e47a0060
sandybradley <sandy.bradley@gmail.com>
2023-08-03 22:20:32 +0200

    feat: revert instead of refund excess deposit

--- src/MevEth.sol
+++ src/MevEth.sol
@@ -490,12 +490,7 @@ contract MevEth is OFTWithFee, IERC4626, ITinyMevEth {
             ERC20(address(WETH)).safeTransferFrom(msg.sender, address(this), assets);
             WETH.withdraw(assets);
         } else {
-            if (msg.value < assets) revert MevEthErrors.DepositTooSmall();
-            if (msg.value > assets) {
-                // refund user excess payment, but in weth to avoid re-entrancy
-                WETH.deposit{ value: msg.value - assets }();
-                ERC20(address(WETH)).safeTransfer(msg.sender, msg.value - assets);
-            }
+            if (msg.value != assets) revert MevEthErrors.WrongDepositAmount();
         }
     }
 
--- test/unit/ERC4626.t.sol
+++ test/unit/ERC4626.t.sol
@@ -104,25 +104,9 @@ contract ERC4626Test is MevEthTest {
         vm.deal(User01, 1.1 ether);
         vm.startPrank(User01);
 
+        vm.expectRevert(MevEthErrors.WrongDepositAmount.selector);
         // Deposit 1 ETH into the mevETH contract with an excess payment
         mevEth.deposit{ value: 1.1 ether }(1 ether, User01);
-
-        // Check that the mevETH contract has 1 ETH
-        assertEq(address(mevEth).balance, 1 ether);
-
-        // Check the user has 1 mevETH
-        assertEq(mevEth.balanceOf(User01), 1 ether);
-
-        // Check the user has 0 ETH
-        assertEq(address(User01).balance, 0 ether);
-
-        // Check the user has 0.1 WETH
-        assertEq(weth.balanceOf(User01), 0.1 ether);
-
-        // Check the fraction has updated correctly
-        (uint256 elastic, uint256 base) = mevEth.fraction();
-        assertEq(elastic, 1 ether);
-        assertEq(base, 1 ether);
     }
 
     function testFuzzSimpleDeposit(uint256 amount) public {
