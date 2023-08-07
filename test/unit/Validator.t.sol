0571859ad44dd0e9275b9e9764f9f8cfef764981
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-08-07 08:28:11 -0600

    Fix MANETH-35 (#151)
    * added new assertion
    
    * added new assertion
    
    * fmt + final negative test
    
    * fmt + final negative test
    
    ---------
    
    Co-authored-by: Sandy Bradley <sandy.bradley@gmail.com>

--- test/unit/Validator.t.sol
+++ test/unit/Validator.t.sol
@@ -22,32 +22,49 @@ contract ValidatorTest is MevEthTest {
 
         //Cache the balance before validator creation, create the validator, and check the balance after
         uint256 balanceBeforeCreation = address(mevEth).balance;
-
         address stakingModule = address(mevEth.stakingModule());
+        bytes32 depositRoot = latestDepositRoot();
         vm.expectEmit(true, false, false, true, address(mevEth));
         emit ValidatorCreated(stakingModule, validatorData);
         vm.prank(Operator01);
-        mevEth.createValidator(validatorData);
+        mevEth.createValidator(validatorData, depositRoot);
 
         uint256 balanceAfterCreation = address(mevEth).balance;
         assertEq(stakingModuleDepositSize, balanceBeforeCreation - balanceAfterCreation);
     }
 
     /**
+     * Ensures that the Operator cannot front-run a validator creation to register an existing validato
+     */
+    function testValidatorIsNotFrontrun() public {
+        IStakingModule.ValidatorData memory data = mockValidatorData(User01, 32 ether / 1 gwei);
+        bytes32 depositRoot = latestDepositRoot();
+        uint256 stakingModuleDepositSize = mevEth.stakingModule().VALIDATOR_DEPOSIT_SIZE();
+        vm.startPrank(Operator01);
+        vm.deal(Operator01, 96 ether);
+
+        depositContract.deposit{ value: 32 ether }(data.pubkey, abi.encodePacked(data.withdrawal_credentials), data.signature, data.deposit_data_root);
+        mevEth.deposit{ value: stakingModuleDepositSize * 2 }(stakingModuleDepositSize * 2, User01);
+
+        vm.expectRevert(MevEthErrors.DepositWasFrontrun.selector);
+        mevEth.createValidator(data, depositRoot);
+    }
+
+    /**
      * Tests validator creation failure cases. This function should revert when the caller is unauthorized or when the contract does not have enough eth.
      */
-
     function testNegativeCreateValidator() public {
         IStakingModule.ValidatorData memory validatorData = mockValidatorData(User01, 32 ether / 1 gwei);
 
+        bytes32 depositRoot = latestDepositRoot();
         //Expect a revert when the caller is unauthorized
         vm.expectRevert(Auth.Unauthorized.selector);
-        mevEth.createValidator(validatorData);
+        mevEth.createValidator(validatorData, depositRoot);
 
         //Expect a revert when the contract does not have enough eth
         vm.prank(Operator01);
         vm.expectRevert(MevEthErrors.NotEnoughEth.selector);
-        mevEth.createValidator(validatorData);
+        mevEth.createValidator(validatorData, depositRoot);
     }
 
     /**
@@ -68,12 +85,14 @@ contract ValidatorTest is MevEthTest {
         vm.prank(User01);
         mevEth.deposit{ value: depositSize * 2 }(depositSize * 2, User01);
 
+        bytes32 depositRoot = latestDepositRoot();
+
         vm.prank(Operator01);
         vm.expectEmit(true, true, true, true, address(wagyuStakingModule));
         emit NewValidator(
             validatorData.operator, validatorData.pubkey, validatorData.withdrawal_credentials, validatorData.signature, validatorData.deposit_data_root
         );
-        mevEth.createValidator(validatorData);
+        mevEth.createValidator(validatorData, depositRoot);
 
         assertEq(wagyuStakingModule.balance(), depositSize);
         assertEq(wagyuStakingModule.validators(), 1);
