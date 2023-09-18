dbbeba18e0502442c2452c235da7118c265bd435
sandybradley <sandy.bradley@gmail.com>
2023-09-18 16:31:15 +0200

    chore: zero address check for _mevEth in mevethsharevault contructor

--- src/MevEthShareVault.sol
+++ src/MevEthShareVault.sol
@@ -52,7 +52,7 @@ contract MevEthShareVault is Auth, IMevEthShareVault {
     /// @param _mevEth The address of the WETH contract to use for deposits.
     /// @param _protocolFeeTo The address that protocol fees are sent to.
     constructor(address authority, address _mevEth, address _protocolFeeTo) Auth(authority) {
-        if (_protocolFeeTo == address(0) || authority == address(0)) {
+        if (_protocolFeeTo == address(0) || authority == address(0) || _mevEth == address(0)) {
             revert MevEthErrors.ZeroAddress();
         }
 
