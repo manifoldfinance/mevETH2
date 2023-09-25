35967fafd42c000d879eb849fbe17b70dc83dabf
sam bacha <sam@manifoldfinance.com>
2023-09-25 06:08:37 -0700

    Update IMevEthShareVault.sol

--- src/interfaces/IMevEthShareVault.sol
+++ src/interfaces/IMevEthShareVault.sol
@@ -1,18 +1,24 @@
-// SPDX-License-Identifier: MIT
+/// SPDX-License-Identifier: UPL-1.0
+
 pragma solidity ^0.8.19;
 
 import { MevEthShareVault } from "../MevEthShareVault.sol";
 
+/// @title IMevEthShareVault
+/// @author CommodityStream, Inc.
+/// @author Manifold Finance, Inc.
+
 interface IMevEthShareVault {
-    /* The receive function handles mev/validator payments. If if the msg.sender is the block.coinbase, 
+    /* 
+    The receive function handles mev/validator payments. If if the msg.sender is the block.coinbase, 
     a ValditorPayment should be emitted and the fees/profits should be updated based on the median validator payment. 
     Otherwise, a MevPayment should be emitted and the fees/profits should be updated based on the medianMevPayment.
     */
 
-    // Function to send rewards to MevEth Contract. In the case of failure, this function sends the funds to the Admin as a fallback.
     /**
-     * @notice This function is used to pay rewards to users.
-     * @dev This function is triggered by the owner of the contract and is used to pay rewards to users.
+     * @notice This function is used to pay rewards
+     * @dev This function is triggered by the owner of the contract and is used to pay rewards to the MevEth Contract.  In the case of failure, this function sends the funds to the Admin as a fallback.
+     * 
      * It is important to note that this function should only be triggered by the owner of the contract.
      * Any other user should not be able to trigger this function.
      */
@@ -28,27 +34,23 @@ interface IMevEthShareVault {
      */
     function fees() external view returns (uint128);
     /**
-     * @notice rewards()
+     * @notice rewards() This function allows users to view the rewards they have earned.
      *
-     * This function allows users to view the rewards they have earned.
-     *
-     * @dev
-     * rewards() is a view function that returns the rewards earned by the user. It is an external function and does not modify the state of the contract.
+     * @dev rewards() is a view function that returns the rewards earned by the user. It is an external function and does not modify the state of the contract.
      */
     function rewards() external view returns (uint128);
 
-    // Function to update the protocol balance, allocating to the fees and rewards
     /**
-     * @notice This function logs the rewards for the protocol.
+     * @notice Function to update the protocol balance, allocating to the fees and rewards for the protocol.
      * @dev This function logs the rewards for the protocol. It takes in the protocol fees owed as an argument.
      */
     function logRewards(uint128 protocolFeesOwed) external;
 
-    // Admin controls
+    //! @notice Admin controls
     function recoverToken(address token, address recipient, uint256 amount) external;
-    // Send the protocol fees to the `feeTo` address
+
     /**
-     * @dev Function to send fees to the contract owner.
+     * @dev Function to send fees to the contract owner. MUST send the protocol fees to the `feeTo` address
      * @notice This function should only be called by the contract owner.
      * @param None
      * @return None
@@ -57,7 +59,7 @@ interface IMevEthShareVault {
     function setProtocolFeeTo(address newFeeTo) external;
     /**
      * @notice Sets the newMevEth address
-     * @dev This function sets the newMevEth address to the address passed in as an argument. This address will be used to store the MEV-ETH tokens.
+     * @dev This function sets the newMevEth address to the address passed in as an argument.
      */
     function setNewMevEth(address newMevEth) external;
 }
